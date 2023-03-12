0f848c907eb7a2b7b687d7bfa9b827fa0445f959
Dovidovich Anastasia <sidorova88-88@mail.ru>
2023-03-12 21:45:43 +0300

    newMassiv

---
+++ itog/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] NewMassiv(string[] array, int count )
+{    
+    string[] newMass = new string[count];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newMass[newMass.Length - count] = array[i];
+            count--;
+        }
+    }
+    return newMass;    
+}
